5372f6a7cdf815878e85f06985fc002fc4c8943d
jssoscar <785979885jss@163.com>
2017-12-22 23:53:27 +0800

    index--add router config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'mobx-react'
+import createBrowserHistory from 'history/createBrowserHistory';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import { Router } from 'react-router';
+
 import './css/index.scss';
 import App from './App';
+import store from './store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const browserHistory = createBrowserHistory();
+const routingStore = new RouterStore();
+
+const stores = {
+  routing: routingStore,
+  store
+};
+
+const history = syncHistoryWithStore(browserHistory, routingStore);
+
+ReactDOM.render(
+    <Provider {...stores}>
+        <Router history={history}>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
+
 registerServiceWorker();
