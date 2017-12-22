446eac572a5c5511e388ebf6959d6a2a6786601a
jssoscar <785979885jss@163.com>
2017-12-23 00:48:30 +0800

    add router config

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Author			jssoscar
+ * @Date			2017-12-23 00:30:32 
+ * @Version			1.0 
+ * @Description	
+ */
+
+import React, {PureComponent} from 'react'
+import {Route, Switch} from 'react-router'
+
+import Index from '../pages/index'
+
+class CustomerRouter extends PureComponent {
+    render() {
+        return <Switch>
+            <Route exact path="/" component={Index}>
+            </Route>
+        </Switch>
+    }
+}
+
+export default CustomerRouter;
