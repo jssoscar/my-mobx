53dbc7b3d5b08ff07fa4a756668ed52852ba6934
jssoscar <785979885jss@163.com>
2017-12-22 19:55:50 +0800

    add store

---
+++ src/store/user/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author			jssoscar
+ * @Date			2017-12-22 19:50:21 
+ * @Version			1.0 
+ * @Description	
+ */
+
+const userInfo = {
+    name : 'jssoscar',
+    email : '785979885jss@163.com',
+    username : '旭升Oscar'
+};
+
+export default userInfo;
