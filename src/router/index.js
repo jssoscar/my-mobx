73cac14f56a3b7dadac87352d85de2934959bc2f
jssoscar <785979885jss@163.com>
2017-12-24 23:51:36 +0800

    fix route change but view not update

--- src/router/index.js
+++ src/router/index.js
@@ -6,19 +6,21 @@
  */
 
 import React, {PureComponent} from 'react'
-import {Route, Switch} from 'react-router'
+import {Route, Switch, withRouter} from 'react-router'
 import AsyncComponent from 'src/components/async'
 
 const Index = AsyncComponent(() => import('src/pages/index'));
 const Login = AsyncComponent(() => import('src/pages/login'));
+const NotFound = AsyncComponent(() => import('src/pages/404'));
 
 class CustomerRouter extends PureComponent {
     render() {
         return <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/index" component={Index} />
+            <Route component={NotFound} />
         </Switch>
     }
 }
 
-export default CustomerRouter;
+export default withRouter(CustomerRouter);
