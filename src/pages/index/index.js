297665e3b19f75430668ac6d105e4c954dae5b9d
jssoscar <785979885jss@163.com>
2017-12-29 17:31:57 +0800

    add default page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -6,18 +6,15 @@
  */
 
 import React, { Component } from 'react';
-import logo from '../../img/logo.svg';
-import '../../css/App.css';
 import {inject, observer} from 'mobx-react'
+import Header from './Header'
 
 @inject('userStore')
 @observer
 class App extends Component {
   render() {
-    return <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+    return <div>
+        <Header />
       </div>
   }
 }
