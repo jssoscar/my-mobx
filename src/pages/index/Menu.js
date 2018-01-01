32ea638b5d7f567a412b7f91f977094071d1bbd7
jssoscar <785979885jss@163.com>
2018-01-01 11:08:53 +0800

    add menu component
    add menu component

---
+++ src/pages/index/Menu.js
@@ -0,0 +1,15 @@
+/*
+ * @Author			jishengsheng
+ * @Date			2017-12-29 18:14:21 
+ * @Version			1.0 
+ * @Description	
+ */
+
+import React from 'react'
+import {Menu} from 'antd';
+
+class AccountMenu extends React.Component {
+
+}
+
+export default AccountMenu;
