04d5e3fb072490f751cbf4fbeb614a169ad43f37
jssoscar <785979885jss@163.com>
2018-01-02 18:55:05 +0800

    add mock rules

---
+++ mock/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author			jssoscar
+ * @Date			2018-01-02 16:00:49 
+ * @Version			1.0 
+ * @Description	
+ */
+
+module.exports = {
+    rules : [
+        {
+            pattern : /\/common\/menu/,
+            response : 'common/menu.json',
+            sleep : 1
+        }
+    ]
+}
