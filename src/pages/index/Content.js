21bd81c4daed9a00e965e55cdc93ef2e402b7a56
jssoscar <785979885jss@163.com>
2018-01-02 20:02:29 +0800

    add content component

---
+++ src/pages/index/Content.js
@@ -0,0 +1,23 @@
+/*
+ * @Author			jssoscar
+ * @Date			2017-12-31 21:10:54 
+ * @Version			1.0 
+ * @Description	
+ */
+
+import React, {Component} from 'react';
+import {Switch} from 'react-router-dom'
+
+class Content extends Component{
+    render() {
+        return <div className="content">
+           <div className="content-container">
+               <Switch>
+                   
+               </Switch>
+           </div>
+        </div>
+    }
+}
+
+export default Content;
