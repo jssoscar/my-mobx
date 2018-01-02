9cb06004d2d8046c87c01b934a68a9f4ed78aec7
jssoscar <785979885jss@163.com>
2018-01-02 20:03:04 +0800

    add menu component

--- src/pages/index/Menu.js
+++ src/pages/index/Menu.js
@@ -6,9 +6,39 @@
  */
 
 import React from 'react'
-import {Menu} from 'antd';
+import {Menu, Layout} from 'antd';
 
+import {observable} from 'mobx'
+import {observer} from 'mobx-react'
+
+const {SubMenu, Item : MenuItem} = Menu;
+const {Sider} = Layout;
+
+@observer
 class AccountMenu extends React.Component {
+
+    @observable openKeys = ['my'];
+
+    onOpenChange = (openKeys) => {
+        this.openKeys = openKeys;
+    }
+
+    render() {
+        const {props} = this;
+
+        const currentPath = props.location.pathname;
+
+        const openKeys = [...this.openKeys];
+
+        return <Sider className="sider-menu">
+            <Menu mode="inline" openKeys={openKeys} onOpenChange={this.onOpenChange}
+                style={{height: '100%'}} defaultSelectedKeys={[currentPath]}>
+                <SubMenu key="my" title="我的工作台">
+                    <MenuItem key="/app/student">个人中心</MenuItem>
+                </SubMenu>
+            </Menu>
+        </Sider>
+    }
 
 }
 
