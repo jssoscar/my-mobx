4260cf5e73d5a1abe8d40c8f22be05e150c351ab
jssoscar <785979885jss@163.com>
2018-01-02 20:02:51 +0800

    header component - add logout entry

--- src/pages/index/Header.js
+++ src/pages/index/Header.js
@@ -1,24 +1,41 @@
 /*
  * @Author			jssoscar
- * @Date			2017-12-29 15:41:00 
- * @Version			1.0 
- * @Description	
+ * @Date			2017-12-23 00:30:32
+ * @Version			1.0
+ * @Description
  */
 
-import React, {Component} from 'react'
-import {inject, observer} from 'mobx-react'
+import React, {Component} from 'react';
+import {Icon, Avatar, Dropdown, Menu} from 'antd';
 
-@inject('userStore')
-@observer
-class Header extends Component {
+const MenuItem = Menu.Item;
+
+class App extends Component {
+
     render() {
+        const {props} = this, {userStore} = props;
+
+        const menu = <Menu selectedKeys={[]}>
+            <MenuItem disabled><Icon type="user"/>个人中心</MenuItem>
+            <MenuItem disabled><Icon type="setting"/>设置</MenuItem>
+            <Menu.Divider/>
+            <MenuItem key="logout" className="menu">
+                <Icon type="logout"/>退出登录
+            </MenuItem>
+        </Menu>
+
         return <div className="header">
-            <div className="logo">
-            </div>
-            <div className="userCenter">
+            <div className="logo"></div>
+            <div className="notice">
+                <Icon type="bell"/>
+                <Dropdown overlay={menu}>
+                    <span className="acount">
+                        <Avatar className="avatar" src={userStore.avatar}/> {userStore.username}
+                    </span>
+                </Dropdown>
             </div>
         </div>
     }
 }
 
-export default Header;
+export default App;
